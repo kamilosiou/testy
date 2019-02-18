586bdd19c25ad737579ff6c5da61b61a23ba020d
kgrzadziel <kamilozor@wp.pl>
2019-02-18 18:17:45 +0100

    Update test_argparse.py

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -2,6 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--language")
+parser.add_argument("--pf_domain")
+parser.add_argument("--browser")
+parser.add_argument("--secure")
+
 args = parser.parse_args()
 
-print args.language
+print args
