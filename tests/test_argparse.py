7fbc7ca6c056bdeac82d99401135f951948804e6
kgrzadziel <kamilozor@wp.pl>
2019-02-19 15:10:19 +0100

    Update test_argparse.py

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -2,7 +2,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--language")
+parser.add_argument("--suite")
 
 args = parser.parse_args()
 
 print args.language
+print args.suite
