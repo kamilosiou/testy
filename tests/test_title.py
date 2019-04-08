a20e35e5704beea3ae123bb54e2c8f3b120b2e1d
kgrzadziel <kamilozor@wp.pl>
2019-04-08 15:45:34 +0200

    Update test_title.py

--- tests/test_title.py
+++ tests/test_title.py
@@ -8,4 +8,5 @@ args, _ = parser.parse_known_args()
 
 browser = webdriver.Chrome()
 browser.get('https://www.goal.com/{0}'.format(args.language))
+print(browser.title)
 assert bool(browser.title) == True
