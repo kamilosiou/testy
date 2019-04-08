eaab6c840b59c48f1d38a38c84c7986a2c0a89b8
kgrzadziel <kamilozor@wp.pl>
2019-04-08 19:07:14 +0200

    Update test_title.py

--- tests/test_title.py
+++ tests/test_title.py
@@ -4,9 +4,11 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--language', default='en')
+parser.add_argument('--suite', default='home_page_tests/test_suite.py')
 args, _ = parser.parse_known_args()
 
 browser = webdriver.Chrome()
 browser.get('https://www.goal.com/{0}'.format(args.language))
+print(args.suite)
 print(browser.title)
 assert bool(browser.title) == True
