815bea080e35a86203b5885251667d720d922a3b
kgrzadziel <kamilozor@wp.pl>
2019-04-08 19:13:28 +0200

    Delete test_argparse.py

--- test_argparse.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--language")
-parser.add_argument("--pf_domain")
-parser.add_argument("--env")
-args = parser.parse_args()
-
-print args
