a6667320750bce78c9b6d819fdaa552d5dc30feb
kgrzadziel <kamilozor@wp.pl>
2019-04-19 09:10:22 +0200

    Update test_title.py

--- tests/test_title.py
+++ tests/test_title.py
@@ -7,7 +7,7 @@ parser.add_argument('--lang', default='en')
 parser.add_argument('--suite', default='home_page_tests/test_suite.py')
 args, _ = parser.parse_known_args()
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome('/usr/local/bin/chromedriver')
 browser.get('https://www.goal.com/{0}'.format(args.language))
 print(args.suite)
 print(browser.title)
